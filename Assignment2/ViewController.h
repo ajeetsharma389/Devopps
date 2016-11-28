83fa03737f87f5aa2e462f06aa4ed18c4026f090
ajeetsharma389 <ajeetsharma389@gmail.com>
2016-11-28 15:18:43 +0530

    Added files via upload as first commit

---
+++ Assignment2/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Assignment2
+//
+//  Created by Ajeet on 16/08/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Assignment2/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  Assignment2
+//
+//  Created by Ajeet on 16/08/16.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
